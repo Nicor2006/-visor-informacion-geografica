18094faad7174e8813ff891c0250b307cfb8e050
Nicolas <nicolasrpv02@gmail.com>
2025-02-14 15:38:35 -0500

    Mapa centrado en el municipio de Repelon

--- src/components/mapComponent/map-control.js
+++ src/components/mapComponent/map-control.js
@@ -1,18 +1,17 @@
-import 'leaflet/dist/leaflet.css';
-import './map.scss';
+import "leaflet/dist/leaflet.css";
+import "./map.scss";
 
-const L =require ('leaflet');
+const L = require("leaflet");
 
-import {carto_light} from './layers/control-layers'
+import { carto_light } from "./layers/control-layers";
 
-export var map = L.map('map', {
-    center: [7.905739, -72.511281],
-    zoom: 13,
-    layers: [carto_light]
+export var map = L.map("map", {
+  center: [10.4944, -75.1242],
+  zoom: 15,
+  layers: [carto_light],
 });
 
-L.control.zoom({position: 'topright'}).addTo(map);
+L.control.zoom({ position: "topright" }).addTo(map);
 
 // scale control
-new L.control.scale({imperial: false}).addTo(map)
-
+new L.control.scale({ imperial: false }).addTo(map);
