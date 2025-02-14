0e5dc8422e1d941a64d26d8645be22fd14f2d650
Nicolas <nicolasrpv02@gmail.com>
2025-02-14 16:53:32 -0500

    Se agrego un marcador al centro del municipio

--- src/components/mapComponent/map-control.js
+++ src/components/mapComponent/map-control.js
@@ -2,9 +2,11 @@ import "leaflet/dist/leaflet.css";
 import "./map.scss";
 import "leaflet.fullscreen/Control.FullScreen.css";
 import "leaflet.fullscreen";
+import { AwesomeMarkersIcon } from "./controls/icons/famIcon";
 
 const L = require("leaflet");
 import { mapa_topografico } from "./layers/control-layers";
+import { dynamicMarker } from "./controls/markers";
 
 // Inicializar el mapa principal
 export var map = L.map("map", {
@@ -21,3 +23,9 @@ new L.control.scale({ imperial: false }).addTo(map);
 
 //Control de pantalla completa
 L.control.fullscreen({ position: "topleft" }).addTo(map);
+
+//Se crea el marcador
+const awesomeIcon = AwesomeMarkersIcon("fa", "heart", "red");
+
+//Marcador en el centro de Replon
+dynamicMarker(awesomeIcon, [10.4935, -75.124], 25).addTo(map);
